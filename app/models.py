4c31edc06c6b75f439e94040bcd3888c1fb13460
daisychelangat <daisyrop2000@gmail.com>
2021-04-17 15:22:46 +0300

    created class news

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,title,image,description,date,article):
+        self.title = title
+        self.image = image
+        self.description = description
+        self.date = date
+        self.article = article
